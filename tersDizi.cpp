da44a29175dd1ca77163262ddabc8f0f36910047
esmabru <esmaebruozturk@gmail.com>
2025-04-27 13:06:23 +0300

    Add files via upload
    Bu program, kullanıcıdan bir dizi boyutu ve dizi elemanlarını alır. Ardından, girilen diziyi tersten ekrana yazdırır.

---
+++ tersDizi.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,dizi[10],boyut;
+    
+    cout << "Dizi boyutunu girin (en fazla 10): ";
+    cin >> boyut;
+
+    cout << "Dizi elemanlarýný giriniz:" << endl;
+    for (i = 0; i < boyut; i++) {
+        cout << "dizi[" << i << "] = ";
+        cin >> dizi[i];
+    }
+
+    cout << "Dizinin tersten yazýlýþý: ";
+    for (i = boyut - 1; i >= 0; i--) {
+        cout << dizi[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
