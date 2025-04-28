85e5bd9e60e3490356c1aaac5a1d0c2da60d0649
esmabru <esmaebruozturk@gmail.com>
2025-04-28 23:00:57 +0300

    Add files via upload
    Verilen hak ile doğru şifreyi tahmin etme programı

---
+++ sifreTahmini.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+	setlocale(LC_ALL, "Turkish");
+	int sifre=123;
+	int tahmin;
+	int hak=3;
+	while(true){
+		cout<<"Þifreyi tahmin ediniz:";
+		cin>>tahmin;
+		if(sifre==tahmin){
+			cout<<"Doðru tahmin.";
+			break;
+		}
+		else{
+			cout<<"Yanlýþ tahmin"<<endl;
+			hak--;
+			cout<<""<<hak;
+			cout<<"Hakkýnýz kaldý"<<endl;
+			if(hak==0){
+				cout<<"Hakkýnýz bitmiþtir.";
+				break;
+			}
+		}
+	}
+}
